7a623bdfa7ac390d43ceeea21d31adce622ac540
miguelalmeidaholanda@gmail.com <miguelalmeidaholanda@gmail.com>
2026-06-16 14:58:32 -0300

    subindo ex04

---
+++ ex04.js
@@ -0,0 +1,17 @@
+let ns = [340, 325, 432, 234, 253, 122]
+let maior = 0
+
+for(let i = 0; i<ns.length; i++ ){
+    if(ns[i]>maior){
+    maior = ns[i]
+    }else{}
+}
+
+console.log("")
+console.log("__________________________________________")
+console.log("")
+console.log(`  ~MAIOR NUMERO ENTRE; ${ns}`)
+console.log("~~~~")
+console.log(`                        ${maior}`)
+console.log("__________________________________________")
+console.log("")

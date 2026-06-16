7586e8f8d397d980b571d93274e866f2b31953ee
miguelalmeidaholanda@gmail.com <miguelalmeidaholanda@gmail.com>
2026-06-16 14:59:13 -0300

    subindo ex05

---
+++ ex05.js
@@ -0,0 +1,17 @@
+let ns = [340, 325, 432, 234, 253, 122]
+let menor = ns[0]
+
+for(let i = 0; i<ns.length; i++ ){
+    if(ns[i]<menor){
+    menor = ns[i]
+    }else{}
+}
+
+console.log("")
+console.log("__________________________________________")
+console.log("")
+console.log(`  ~MENOR NUMERO ENTRE; ${ns}`)
+console.log("~~~~")
+console.log(`                        ${menor}`)
+console.log("__________________________________________")
+console.log("")
